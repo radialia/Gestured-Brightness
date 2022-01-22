8fd9f65c7a960fe8feadd9b4576ca52ce9e7ed83
Hardik Jaiswal <96730754+radialia@users.noreply.github.com>
2022-01-22 10:58:26 +0530

    Added: Multithreading and file directories

---
+++ src/FPS.py
@@ -0,0 +1,25 @@
+# import the necessary packages
+import datetime
+
+
+class FPS:
+    def __init__(self):
+        # store the start time, end time, and total number of frames
+        # that were examined between the start and end intervals
+        self._start = None
+        self._end = None
+        self._numFrames = 0
+
+    def start(self):
+        # start the timer
+        self._start = datetime.datetime.now()
+        return self
+
+    def stop(self):
+        # stop the timer
+        self._end = datetime.datetime.now()
+
+    def update(self):
+        # increment the total number of frames examined during the
+        # start and end intervals
+        self._numFrames += 1
--- src/main.py
+++ src/main.py
@@ -1,41 +1,53 @@
-# Importing modules
+# Importing required modules
+from imutils.video import WebcamVideoStream
+from imutils.video import FPS
+import imutils
 import cv2
+import time
 import math
 import numpy as np
 import screen_brightness_control as sbc
 import HandDetector
 
-# Initialise camera object
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-
-# Set the width and height of the opencv window
-cap.set(3, 800)
-cap.set(4, 500)
+# Initialise the camera object
+vs = WebcamVideoStream(src=0).start()
+# Starting the fps
+fps = FPS().start()
+pTime = 0
 
 # Initialise hand detection
 detector = HandDetector.Detector()
 
+while True:
+    # Read the frame
+    frame = vs.read()
+    # Flip the frame to give a selfie view
+    frame = cv2.flip(frame, 1)
+    # Resize the frame's width to 600px
+    frame = imutils.resize(frame, width=600)
 
-while cap.isOpened():
-    # Reads the camera
-    success, video = cap.read()
-    if not success:
-        print("Ignoring empty camera frame....")
-        continue
+    # Get the height, width and channel from the frame
+    height, width, channel = frame.shape
 
-    # Flip the screen to give a selfie view
-    video = cv2.flip(video, 1)
+    # Calculating the fps
+    cTime = time.time()
+    _fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    # Putting the FPS text on the frame
+    cv2.putText(frame, f"FPS: {int(_fps)}", (width-150, 50),
+                cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
 
     # Show brightness text on the screen
-    cv2.putText(video, f"Brightness: {int(sbc.get_brightness())}%",
-                (40, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (228, 0, 63), 2)
+    cv2.putText(frame, f"Brightness: {int(sbc.get_brightness())}%",
+                (40, 50), cv2.FONT_HERSHEY_PLAIN, 2, (228, 0, 63), 2)
 
     # Show instruction to quit the window
-    cv2.putText(video, f"Press 'q' to exit",
-                (40, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (10, 0, 18), 2)
+    cv2.putText(frame, f"Press 'q' to exit",
+                (40, 80), cv2.FONT_HERSHEY_PLAIN, 2, (10, 0, 18), 2)
 
     # Finds index of each landmark on the recognised hand
-    landmarkList = detector.findIndex(video)
+    landmarkList = detector.findIndex(frame)
     # Get which fingers are raised up
     fingers = detector.fingerIsUp()
 
@@ -56,14 +68,14 @@ while cap.isOpened():
                 center_x, center_y = (thumb_x+index_x)//2, (thumb_y+index_y)//2
 
                 # Draw circles on the indexes and a line in between them
-                cv2.circle(video, (thumb_x, thumb_y),
+                cv2.circle(frame, (thumb_x, thumb_y),
                            10, (20, 63, 255), cv2.FILLED)
-                cv2.circle(video, (index_x, index_y),
+                cv2.circle(frame, (index_x, index_y),
                            10, (20, 63, 255), cv2.FILLED)
-                cv2.line(video, (thumb_x, thumb_y),
+                cv2.line(frame, (thumb_x, thumb_y),
                          (index_x, index_y), (20, 241, 255), 3)
                 # Draw a circle in the middle
-                cv2.circle(video, (center_x, center_y),
+                cv2.circle(frame, (center_x, center_y),
                            5, (229, 45, 0), cv2.FILLED)
 
                 # Get the distance between the index finger and thumb using distance formula
@@ -75,11 +87,19 @@ while cap.isOpened():
                 # Set the brightness accordingly
                 sbc.set_brightness(brightness, force=True)
 
-    # Show the window
-    cv2.imshow('Gestured Brightness', video)
-    # Press 'q' to quit window
-    if cv2.waitKey(5) & 0xFF == ord('q'):
+    # Show the frame in a window
+    cv2.imshow("Gestured Brightness Control", frame)
+    # Updates the frame simulataneously
+    fps.update()
+
+    # If the 'q' key is pressed then exit the window
+    if(cv2.waitKey(5) & 0xFF == ord('q')):
         break
 
-# Release the camera
-cap.release()
+# Stop the fps
+fps.stop()
+
+# destroy all the windows
+cv2.destroyAllWindows()
+# Stop recording
+vs.stop()
